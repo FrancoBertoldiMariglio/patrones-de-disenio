8f99b8e1de9a178c2501773985593ef5b9bb821d
FrancoBertoldiMariglio <102602471+FrancoBertoldiMariglio@users.noreply.github.com>
2023-11-04 16:35:46 -0400

    Delete Patron Observer directory

--- Patron Observer/Dashboard.py
+++
@@ -1,6 +0,0 @@
-from Observer import Observer
-
-
-class Dashboard(Observer):
-    def update(self, temperature):
-        print(f"El panel de control muestra la temperatura: {temperature}°C")
--- Patron Observer/WeatherApp.py
+++
@@ -1,6 +0,0 @@
-from Observer import Observer
-
-
-class WeatherApp(Observer):
-    def update(self, temperature):
-        print(f"La aplicación móvil muestra la temperatura: {temperature}°C")
